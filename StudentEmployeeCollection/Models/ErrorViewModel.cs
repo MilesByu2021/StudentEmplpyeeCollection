9d7aa4ff1d6651d9e3b3ec1fa5bc4b2c08563b29
MilesByu2021 <milesleung0724@gmail.com>
2022-10-24 14:02:34 -0600

    First Commit
    I created the web app

---
+++ StudentEmployeeCollection/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StudentEmployeeCollection.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
